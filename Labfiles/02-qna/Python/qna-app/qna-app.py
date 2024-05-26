4c44e4f02c8ac231ecb799db059e924072f7c461
Gabriel Santos Tramontin <145608943+gabrielstramontin@users.noreply.github.com>
2024-05-25 22:14:41 -0300

    Update qna-app.py

--- Labfiles/02-qna/Python/qna-app/qna-app.py
+++ Labfiles/02-qna/Python/qna-app/qna-app.py
@@ -1,28 +1,39 @@
+# First, install the Azure AI Language question answering SDK package [pip install azure-ai-language-questionanswering]
+
 from dotenv import load_dotenv
 import os
 
-# Import namespaces
-
+# Import Namespaces:
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.language.questionanswering import QuestionAnsweringClient
 
 def main():
     try:
-        # Get Configuration Settings
+        # Get Configuration Settings:
         load_dotenv()
         ai_endpoint = os.getenv('AI_SERVICE_ENDPOINT')
         ai_key = os.getenv('AI_SERVICE_KEY')
         ai_project_name = os.getenv('QA_PROJECT_NAME')
         ai_deployment_name = os.getenv('QA_DEPLOYMENT_NAME')
 
-        # Create client using endpoint and key
+        # Create client using Endpoint and Key:
+        credential = AzureKeyCredential(ai_key)
+        ai_client = QuestionAnsweringClient(endpoint=ai_endpoint, credential=credential)
 
-
-        # Submit a question and display the answer
-
-
-
+        # Submit a question and display the answer:
+        user_question = ''
+        while user_question.lower() != 'quit':
+            user_question = input('\nQuestion:\n')
+            response = ai_client.get_answers(question=user_question, project_name=ai_project_name, deployment_name=ai_deployment_name)
+            for candidate in response.answers:
+                print(candidate.answer)
+                print("Confidence: {}".format(candidate.confidence))
+                print("Source: {}".format(candidate.source))
+                
     except Exception as ex:
         print(ex)
 
-
 if __name__ == "__main__":
     main()
+
+# At the end, in the integrated terminal, enter the following command to run the program: [python qna-app.py]
